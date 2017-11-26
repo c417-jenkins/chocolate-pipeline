e86a641c6e01c7760ff9101145495391328f9fe2
c417-jenkins <c417.jenkins@gmail.com>
2017-11-26 21:31:05 +0900

    Modify the stages

--- chocolate_pipeline.groovy
+++ chocolate_pipeline.groovy
@@ -1,14 +1,49 @@
 def gitUrl = "https://github.com/c417-jenkins/chocolate-factory.git"
 def targetBranch = "master"
+
+def contentType = "application/json"
+def postUrl = "http://localhost:8081/c417-factory/api/chocolate"
+
+def setting
 node {
-    stage("checkout") {
+    stage("checkout git") {
       dir("${WORKSPACE}") {
           git url: "${gitUrl}", branch: "${targetBranch}", credentialsId: "${ChocolateJenkins}"
-          println("hello")
+          sh "ls -la"
       }
     }
 
-    stage("finish")
+    stage("Lint") {
+      dir("${WORKSPACE}") {
+        println("TODO: Add lint")
+      }
+    }
+
+    stage("Unit Test") {
+      dir("${WORKSPACE}") {
+        withMaven( maven: "M3") {
+          sh "mvn test"
+        }
+      }
+    }
+
+    stage("build") {
+      dir("${WORKSPACE}") {
+        withMaven( maven: "M3") {
+          sh "mvn install"
+        }
+      }
+    }
+
+    stage("Integration Test") {
+      dir("${WORKSPACE}") {
+        println("TODO: curl")
+      }
+    }
+
+
+    stage("deploy") {
       println("see you")
+    }
 
 }
